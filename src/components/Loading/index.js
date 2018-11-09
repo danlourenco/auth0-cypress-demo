939c0867851ca6ad3ce0a6f729f0f03843fd2c33
Dan Lourenco <danlourenco@gmail.com>
2018-11-08 23:39:48 -0800

    update project, use semantic-ui

--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import Loader from 'react-loader-spinner';
 import styles from './loading.module.scss';
 
 const Loading = () => (
   <div className={ styles.loadingContainer}>
-    <Loader type="ThreeDots" color="#ccc" height={100} width={100} />
+    {'Loading'}
   </div>
 )
 
